3df1d9f4b908b900494ce32a0334b7704804099c
Shreya-Verma1234 <shreyaashu686@gmail.com>
2025-07-26 10:16:22 +0000

    In-depth

--- Basic/dataType.js
+++ Basic/dataType.js
@@ -5,4 +5,10 @@ let one=true;
 console.log(typeof age);
 console.log(typeof name);
 console.log(typeof one);
- 
+
+//symbols
+let s1= Symbol (111);
+let s2=Symbol (111);
+console.log(s1===s2);
+console.log(typeof s1);
+
---
+++ Basic/memory.js
@@ -0,0 +1,26 @@
+// Primitive (Call by value)
+
+let name="shreya";
+let nickName= name;
+console.log(name);
+console.log(nickName);
+nickName="Ashu";
+console.log(name);
+console.log(nickName);
+
+// Non-primitive (Call by reference)
+
+let emp={
+    name:"Ashu",
+    id:1234,
+    address:"JBros"
+}
+let emp2=emp;
+emp2.name="Shreya";
+console.log(emp.name);
+console.log(emp2.name);
+
+
+
+
+
