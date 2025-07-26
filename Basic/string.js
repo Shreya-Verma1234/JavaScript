a9c27a6a9adbc9d1409e8b5ece2fbde36f011488
Shreya-Verma1234 <shreyaashu686@gmail.com>
2025-07-26 12:05:52 +0000

    String and Random

---
+++ Basic/string.js
@@ -0,0 +1,11 @@
+let nickName="Ashu";
+console.log(`My name is${nickName}`);
+let name="Shreya";
+console.log(name.slice(-5,3));
+// slice works as 12345 and then 1->-6, 2->-5, 3->-4 and so on..
+let lName="   verma    ";
+console.log(lName.trimStart());
+console.log(lName.trimEnd());
+
+
+
